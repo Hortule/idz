0ffd28ba1f0ce103afc4e8eed1ed5b4b9a3ce6ed
Hortule <hortule@gmail.com>
2018-11-11 20:00:02 +0300

    2/3 realized

--- labs/lb4.py
+++ labs/lb4.py
@@ -2,9 +2,24 @@ from random import uniform
 
 
 count = int(input("input count of elements N (N <= 30): "))
+c = float(input("input C: "))
 mas = []
 for i in range(0, count):
     mas.append(uniform(-5.0, 5.0))
 print("начальное состояние\n{0}".format(mas))
 
+count2 = 0
+for i in range(0, count):
+    if mas[i] > c:
+        count2 += 1
+print("Count of elements bigger then C: {0}".format(count2))
+
+bn = -1
+for i in range(0, count):
+    if abs(mas[i]) > bn:
+        bn = abs(mas[i])
+        pos = i
+print("Numbers after biggest module: ", end=' ')
+for i in range(i, count):
+    print("{0}".format(mas[i]))
 
